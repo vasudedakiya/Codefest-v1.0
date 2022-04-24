2bb5d01cb97eb97824080565b6ea9189c8f66f35
vasu dedakiya <vasudedakiya1742002@gmail.com>
2022-04-25 01:25:54 +0530

    Telephone Grid Solved

---
+++ Telephone Grid/TelephoneGrid.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+inputs = open("input.txt")
+N = int(inputs.readline())
+result = open("output.txt", "w")
+ans = []
+
+for i in range(0, N, 1):
+    temp = int(inputs.readline())
+    tower = [str(j + 1) for j in range(temp)]
+    per = sorted(permutations(tower), reverse=True)
+
+    for p in per:
+        ans.append("[" + ",".join(p) + "]")
+
+    result.write("".join(ans) + "\n")
