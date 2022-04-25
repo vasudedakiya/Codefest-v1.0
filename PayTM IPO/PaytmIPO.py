388695b3686d29a1695c4c4a01dc577f49dd03d9
vasu dedakiya <vasudedakiya1742002@gmail.com>
2022-04-25 11:39:59 +0530

    PayTM IPO Solved

---
+++ PayTM IPO/PaytmIPO.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+
+inputs = open("input.txt")
+result = open("output.txt", "w")
+N = int(inputs.readline())
+
+for i in range(0, N, 1):
+    arr = list(inputs.readline().replace("[", "").replace("]", "").split(","))
+    arr = [int(x) for x in arr]
+    indx = profit = j = h = 0
+    buy = arr[0]
+    ans = []
+    for price in arr:
+        j += 1
+        if (price - buy) > profit:
+            profit = price - buy
+            indx = j
+
+        if price < buy:
+            buy = price
+
+    time = datetime(2022, 1, 1, 9, 30, 0)
+    time += timedelta(minutes=indx * 10)
+    h = time.hour % 12 if time.hour > 12 else time.hour
+    minute = time.minute
+
+    if minute == 0:
+        minute = "00"
+
+    ans.append(f"[{profit},{h}.{minute}]")
+    print(ans, end="\n")
+    result.write(f"[{profit},{h}.{minute}]" + "\n")
